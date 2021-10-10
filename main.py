8ac06436c0cb096b8aa96f420622e3ce50bc5b7b
Ibraheem Halloum <ibra@supy.io>
2021-10-10 23:42:48 +0300

    edit message

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ data=[]
 
 @app.route('/')
 def hello():
-    di = {"message":'This is Cities List :',"data":data}
+    di = {"message":'This is Cities List :++:',"data":data}
     return jsonify(di)
 
 
