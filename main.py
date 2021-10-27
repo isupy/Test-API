5416d1df819c53a39453fe9d6c21ba27eb22aebe
Ibra Halloum <90552571+isupy@users.noreply.github.com>
2021-10-27 23:48:44 +0300

    Test (#2)
    * Update .gitignore
    
    * all requests
    
    * Create install.yaml
    
    * new commit
    
    * edit message
    
    * edit message
    
    * edit message
    
    * edit message
    
    * edit message
    
    * edit message
    
    * edit message
    
    * edit message
    
    * edit message
    
    * edit message
    
    * edit message
    
    * edit message
    
    * Update .gitignore
    
    * Update .gitignore

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ data=[]
 
 @app.route('/')
 def hello():
-    di = {"message":'Main Cities List main',"data":data}
+    di = {"message":'This is Cities List ::::::',"data":data}
     return jsonify(di)
 
 
